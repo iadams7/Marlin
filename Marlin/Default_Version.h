72961bba913a21056d444a9a5cfd4199fe07051c
Filip Goc <filip@imade3d.com>
2016-11-14 01:42:22 -0500

    Jellybox -> JellyBOX

--- Marlin/Default_Version.h
+++ Marlin/Default_Version.h
@@ -6,8 +6,8 @@
 // #error "You must specify the following parameters related to your distribution"
 
 #if true
-#define SHORT_BUILD_VERSION "JellyBox waking up!"
-#define DETAILED_BUILD_VERSION "Marlin RC3 based firmware for IMADE3D Jellybox"
+#define SHORT_BUILD_VERSION "JellyBOX waking up!"
+#define DETAILED_BUILD_VERSION "Marlin RC3 based firmware for IMADE3D JellyBOX"
 #define STRING_DISTRIBUTION_DATE "v1.0.1 | 2016-09-02"
 // It might also be appropriate to define a location where additional information can be found
 #define SOURCE_CODE_URL  "https://github.com/IMADE3D/Marlin"
